eb76be182525b67cd41071f324dc713edd699d6b
khensura.loveel Love EL <khensura.loveel@gmail.com>
2018-11-20 02:54:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,9 +7,7 @@ function takeANumber( line , name){
 function nowServing(line) {
   if  (line.length === 0) {
     return "There is nobody waiting to be served!";
-  }
-  
-  else {
+  } else {
    line.slice(1);
     return `Currently serving ${line.shift()}.`;
    
